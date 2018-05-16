6b4dece497cad9f0b1896aa141314266c4f3716e
Ralph Schindler <ralph@ralphschindler.com>
2018-05-16 14:38:11 -0500

    added phpunit

---
+++ tests/Unit/Eloquent/EloquentImageryObserverTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ZiffDavis\Laravel\EloquentImagery\Test\Unit\Eloquent;
+
+use PHPUnit\Framework\TestCase;
+
+class EloquentImageryObserverTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
